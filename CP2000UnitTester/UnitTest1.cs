00d8b59cb2bcabdce97805485da22bc043943d54
tgp1994 <tgp1994@gmail.com>
2016-02-17 18:49:32 -0500

    Lets get this party started (tested)
    Creating solution and project files, as well as a unit testing project
    so the main parser can be built as a class library. Also moved the Main
    method from the library into the unit tester. Adding .gitignore for
    Visual Studio fluff.

---
+++ CP2000UnitTester/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CityParser2000;
+
+namespace CP2000UnitTester
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		/// <summary>
+		/// Consider this the "Main" unit test. It's the first one, and is meant to do initial functionality testing.
+		/// </summary>
+		[TestMethod]
+		public void LetsDoThisThing()
+		{
+			CityParser parser = new CityParser();
+
+			City ourCity = parser.ParseBinaryFile("C:\\Users\\Owner\\Desktop\\CitiesSC2000\\dustropolis.sc2");
+			//City ourCity = parser.ParseBinaryFile("C:\\Users\\Owner\\Desktop\\CitiesSC2000\\new city.sc2");
+			//City ourCity = parser.ParseBinaryFile("C:\\Users\\Owner\\Desktop\\CitiesSC2000\\dustropolis.sc2");
+			//City ourCity = parser.ParseBinaryFile("C:\\Users\\Owner\\Desktop\\CitiesSC2000\\altTest2.sc2");
+			//City ourCity = parser.ParseBinaryFile("C:\\Users\\Owner\\Desktop\\CitiesSC2000\\zoneTest.sc2");
+			//City ourCity = parser.ParseBinaryFile("C:\\Users\\Owner\\Desktop\\CitiesSC2000\\underground_test.sc2");
+		}
+	}
+}
